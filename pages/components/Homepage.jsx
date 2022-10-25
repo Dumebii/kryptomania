24a644ef7c09b2ec9fdc301b24874272582a229b
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-25 22:57:40 +0100

    updatee navbar

---
+++ pages/components/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return(
+    <div class="bg-[url('../../public/background.jpg')]">
+
+    </div>
+    )
+}
+
+export default Homepage;
---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import navbrand from '../../public/navbrand.jpg'
+
+function Navigationbar() {
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"><Image src={navbrand} alt="navbrand" width={50} height={50} className='rounded-full' /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className='text-white'>Contact</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Socials</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Twittr</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Instagram</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigationbar;
