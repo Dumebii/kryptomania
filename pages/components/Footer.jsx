dfa09a662461081dbb2e486eea28fde8c5d13ce8
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-26 09:32:58 +0100

    Update footer

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className='flex flex-col gap-10  items-center justify-evenly md:flex-col sm:flex-col mt-20 mb-20'>
         <Image src={navbrand} alt="navbrand" width={50} height={50} className='rounded-full' />
-        <a href="wa.link/5rulrl" 
+        <a href="https://wa.me/message/OM5UPFY3SGRPC1" 
         className='no-underline text-gray-900 lg:text-4xl md:text-4xl'>Contact Us</a>
         <a href="https://twitter.com/Kryptomania9?t=yorSooDP1Z1dpfODelt7Rw&s=09"
          className='no-underline text-gray-900 lg:text-4xl md:text-4xl'>Twitter</a>
