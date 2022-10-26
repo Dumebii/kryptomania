1fb8e864986b78014bb858c7183f551df514b185
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-26 06:19:34 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className=''>
       <Head>
         <title>Kryptomania</title>
-        <meta name="Trade digital currencies" content="Kryptomania is a digital assets brand making the receiving of digital funds easy and without stress. Trade your cryptocurrencies and gift cards with us today and enjoy speed and flexibility with great rates! Here at kryptomania, transparent and seamless transactions are what we do " />
+        <meta name="Trade digital currencies" content="Kryptomania is a digital assets brand making the receiving of digital funds easy and without stress." />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
