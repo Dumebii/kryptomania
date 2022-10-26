4e1194f02fb7a6cc333810567c35d44e98b6191d
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-26 09:03:44 +0100

    Update Buttons

--- pages/components/Buttons.jsx
+++ pages/components/Buttons.jsx
@@ -8,7 +8,7 @@ const Buttons = () => {
   return (
     <div >
         <div className='flex gap-10 lg:ml-[400px] md:ml-10 justify-around mt-10 mb-10'>
-            <button className='bg-blue-600 hover:bg-orange-500 rounded-2xl md:text-2xl pb-2 pr-5
+            <button className='bg-blue-600 hover:bg-orange-500 rounded-2xl md:text-2xl pb-2 pr-7
             p-2 lg:text-2xl text-sm text-white'>
             <a className='no-underline text-white' href='wa.link/5rulrl'>
               <FaWhatsapp className='inline text-lime-300 md:text-2xl lg:text-2xl text-sm' /> Whatsapp
@@ -21,7 +21,7 @@ const Buttons = () => {
              </a>
             </button>
             <button className='bg-blue-600 hover:bg-orange-500 rounded-2xl 
-            md:text-2xl p-2 pb-3 lg:text-2xl text-sm text-white'>
+            md:text-2xl p-2 lg:text-2xl text-sm text-white'>
                 <a href='https://instagram.com/trade_with_kryptomania?igshid=YmMyMTA2M2Y=' className='no-underline text-white'>
                 <BsInstagram className='inline text-pink-800 text-sm md:text-2xl lg:text-2xl' /> Instagram 
                 </a>
