43a3ccf35b0115dd345674b6211153dc6f6bbb67
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-26 01:53:56 +0100

    change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Buttons from './components/Buttons'
 import Carousels from './components/Carousel'
 import Footer from './components/Footer'
